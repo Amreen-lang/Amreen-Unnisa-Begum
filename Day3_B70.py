cde66d51c2f13571f4d98aa0a91edc9bb2de4d27
Amreen-lang <75995225+Amreen-lang@users.noreply.github.com>
2022-10-14 00:03:23 +0530

    Day3_B70

---
+++ Day3_B70.py
@@ -0,0 +1,328 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+continuation with strings : 
+
+
+# In[ ]:
+
+
+
+
+
+# In[1]:
+
+
+first_name = 'Amreen'
+
+last_name = 'Unnisa'
+
+
+# In[ ]:
+
+
+# req : i want to get the full name...?
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+introduction to f strings....!
+
+
+# In[ ]:
+
+
+General syntax of f strings
+
+
+# In[ ]:
+
+
+f"custom words  {placeholder -1} {placeholder2}......{placeorder..n}"
+
+
+# In[ ]:
+
+
+
+
+
+# In[2]:
+
+
+full_name = f"{first_name} {last_name}"
+
+print(full_name)
+
+
+# In[3]:
+
+
+print(full_name.title())
+
+
+# In[4]:
+
+
+message = f"keep up the good work, {full_name.title()}"
+
+print(full_name)
+
+
+# In[5]:
+
+
+print(message)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+Adding whitespaces to strings :
+
+
+# In[ ]:
+
+
+# req : 
+
+
+# In[6]:
+
+
+print("favourite_language:pythonjavac++swiftcobal")
+
+
+# In[7]:
+
+
+print("favourite_language:\npython\njava\nc++\nswift\ncobal")
+
+
+# In[ ]:
+
+
+\n ====> new line delimiter
+
+
+# In[8]:
+
+
+print("favourite_language:\n\tpython\n\tjava\n\tc++\n\tswift\n\tcobal")
+
+
+# In[ ]:
+
+
+\t ====> tab delimiter
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+how to remove whitespaces from the strings :
+
+
+# In[9]:
+
+
+name = 'python'
+
+print(name)
+
+
+# In[ ]:
+
+
+
+
+
+# In[10]:
+
+
+name2 = '  python'
+
+print(name2)
+
+
+# In[ ]:
+
+
+
+
+
+# In[11]:
+
+
+name3 = 'python  '
+
+print(name3)
+
+
+# In[ ]:
+
+
+
+
+
+# In[12]:
+
+
+name2.lstrip()   #left side stripping
+
+
+# In[13]:
+
+
+name3.rstrip()   #right side elemenation
+
+
+# In[14]:
+
+
+name4.strip()
+
+
+# In[15]:
+
+
+name4 = "  python  "
+
+print(name4)
+
+
+# In[16]:
+
+
+name4.strip()   #search operations ----> elemations
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
